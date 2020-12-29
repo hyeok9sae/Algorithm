9665245fab117707a91cdf2dcafe528d8fe7c344
Jaehyeok <hyeok9sae@gmail.com>
2020-12-30 00:24:55 +0900

    20.12.30

--- BOJ_Python/BOJ_1406.py
+++ BOJ_Python/BOJ_1406.py
@@ -1,43 +1,26 @@
 # 에디터
-import sys
-input = sys.stdin.readline
 from collections import deque
-# print = sys.stdout.write
-class Editor:
-    def __init__(self, s):
-        self.lst = deque()
-        self.lst.extend(s)
-        self.cursor = len(self.lst)-1
-    def L(self):
-        if self.cursor == -1:
-            return
-        self.cursor -= 1
-    def D(self):
-        if self.cursor == len(self.lst)-1:
-            return
-        self.cursor += 1
-    def B(self):
-        if self.cursor == -1:
-            return
-        self.lst.pop(self.cursor)
-        self.cursor -= 1
-    def P(self, S):
-        self.lst.insert(self.cursor+1, S)
-        self.cursor += 1
-
-s = input().strip()
-n = int(input())
-ed = Editor(s)
-for _ in range(n):
-    msg = input().split()
-    if msg[0] == 'L':
-        ed.L()
-    if msg[0] == 'D':
-        ed.D()
-    if msg[0] == 'B':
-        ed.B()
-    if msg[0] == 'P':
-        ed.P(msg[1])
-
-for i in ed.lst:
-    print(i, end='')
+deq1, deq2 = deque(), deque()
+inp = input()
+for i in inp:
+    deq1.append(i)
+N = int(input())
+for _ in range(N):
+    msg = list(input().split())
+    if msg[0] == "L":
+        if len(deq1) == 0:
+            continue
+        deq2.appendleft(deq1.pop())
+    if msg[0] == "D":
+        if len(deq2) == 0:
+            continue
+        deq1.append(deq2.popleft())
+    if msg[0] == "B":
+        if len(deq1) == 0:
+            continue
+        deq1.pop()
+    if msg[0] == "P":
+        deq1.append(msg[1])
+deq1 += deq2
+# print(''.join(deq1))
+print(''.join(deq1))
