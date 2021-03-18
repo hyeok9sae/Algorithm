c1f93466626a70ca1de30e3f1e1059f8fcda1301
Jaehyeok <hyeok9sae@gmail.com>
2021-03-18 23:33:16 +0900

    21.03.18

--- BOJ_Python/BOJ_2609.py
+++ BOJ_Python/BOJ_2609.py
@@ -1,14 +1,16 @@
 # 최대공약수와 최소공배수
-def gcd(a, b):
+def gcd(num1, num2):
     while True:
-        if b == 0:
-            return a
-        tmp = a
-        a = b
-        b = tmp%b
-def lcm(a, b, c):
-    return a*b//c
-a, b = map(int, input().split())
-c = gcd(a, b)
-print(c)
-print(lcm(a, b, c))
+        tmp = num1 % num2
+        num1 = num2
+        num2 = tmp
+        if tmp == 0:
+            break
+    return num1
+
+def lcm(num1, num2, g):
+    return g * num1 // g * num2 // g
+
+A, B = map(int, input().split())
+print(gcd(A, B))
+print(lcm(A, B, gcd(A, B)))
