fc0a889c962495dd775f0f2b5dce006f8e640a35
jaehyeok <hyeok9sae@gmail.com>
2021-04-02 16:34:06 +0900

    21.04.02

--- Programmers_Java/2019 KAKAO/src/lv2오픈채팅방/Solution.java
+++ Programmers_Java/2019 KAKAO/src/lv2오픈채팅방/Solution.java
@@ -1,5 +1,9 @@
 package lv2오픈채팅방;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class Solution {
     public static void main(String[] args) {
         String[] record = {"Enter uid1234 Muzi", "Enter uid4567 Prodo","Leave uid1234","Enter uid1234 Prodo","Change uid4567 Ryan"};
@@ -7,6 +11,32 @@ public class Solution {
     }
     public static String[] solution(String[] record){
         String[] answer = {};
+        HashMap<String, String> hm = new HashMap<>();
+        HashMap<String, String> hm2 = new HashMap<>();
+        List<String> lst = new ArrayList<>();
+        List<String> ans = new ArrayList<>();
+        for (int i = 0; i < record.length; i++) {
+            String[] tmp = record[i].split(" ");
+            if (tmp[0].equals("Enter")) {
+                hm.put(tmp[0], "님이 들어왔습니다.");
+                hm2.put(tmp[1], tmp[2]);
+                lst.add(tmp[1] + " " + hm.get(tmp[0]));
+            } else if (tmp[0].equals("Leave")) {
+                hm.put(tmp[0], "님이 나갔습니다.");
+                lst.add(tmp[1] + " " + hm.get(tmp[0]));
+            } else if (tmp[0].equals("Change")) {
+                hm2.replace(tmp[1], tmp[2]);
+            }
+        }
+        for (String s : lst){
+            String[] ss = s.split(" ");
+            ans.add(hm2.get(ss[0])+ss[1]+" "+ss[2]);
+        }
+        answer = new String[ans.size()];
+        ans.toArray(answer);
+//        for (String i : answer){
+//            System.out.println(i);
+//        }
         return answer;
     }
 }
--- Programmers_Java/2019 KAKAO/src/lv2후보키/Solution.java
+++ Programmers_Java/2019 KAKAO/src/lv2후보키/Solution.java
@@ -1,22 +1,19 @@
 package lv2후보키;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
 public class Solution {
+    static HashSet<Integer> hs;
+
     public static void main(String[] args) {
-        String[][] relation = {{"100","ryan","music","2"},{"200","apeach","math","2"},{"300","tube","computer","3"},{"400","con","computer","4"},{"500","muzi","music","3"},{"600","apeach","music","2"}};
-        int answer = solution(relation);
-    }
-    public static int solution(String[][] relation){
-        int answer = 0;
-        int attSize = relation[0].length;
-        int[] arr = new int[attSize];
-        for (int i = 0; i < attSize; i++){
-            arr[i] = i;
+        hs = new HashSet<>();
+        for (int i = 0; i < 5; i++){
+            hs.add(i);
         }
-        boolean[] visited = new boolean[attSize];
-        for (int r = 0; r < arr.length-1; r++){
-            comb(arr, visited);
-        }
-        return answer;
+        hs.add(6);
+        hs.add(3);
+        System.out.println(hs);
     }
-    public static void
 }
