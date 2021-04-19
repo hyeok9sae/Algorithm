fba1b99e21d8feea9703a9e3764c6605957faaee
jaehyeok <hyeok9sae@gmail.com>
2021-04-19 11:27:28 +0900

    21.04.19

--- BOJ_Python/BOJ_1248.py
+++ BOJ_Python/BOJ_1248.py
@@ -1,15 +1,17 @@
 # 맞춰봐
+import sys
 def perm(r):
-    if r == N:
-        print(tmp)
+    global flag
+    if flag:
         return
     if len(tmp) != 0:
-        res = 0
         for i in range(len(tmp)):
+            res = tmp[i]
             for j in range(len(tmp)):
                 if i > j:
                     continue
-                res += tmp[j]
+                if i != j:
+                    res += tmp[j]
                 if matrix[i][j] == '-':
                     if res >= 0:
                         return
@@ -19,15 +21,15 @@ def perm(r):
                 elif matrix[i][j] == '+':
                     if res <= 0:
                         return
+    if r == N:
+        print(*tmp)
+        flag = True
+        return
 
     for idx, i in enumerate(num_lst):
-        if visited[idx]:
-            continue
         tmp.append(i)
-        visited[idx] = True
         perm(r+1)
         tmp.pop()
-        visited[idx] = False
 
 N = int(input())
 s = input()
@@ -39,13 +41,11 @@ for i in range(N):
             continue
         matrix[i][j] = s[pn]
         pn+=1
-print(matrix)
 num_lst = [-10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 visited = [False]*len(num_lst)
 tmp = []
+flag = False
 for idx, i in enumerate(num_lst):
     tmp.append(i)
-    visited[idx] = True
     perm(1)
     tmp.pop()
-    visited[idx] = False
