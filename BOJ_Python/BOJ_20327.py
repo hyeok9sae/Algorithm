b2e06f584aeb32d0d41447c2dc6bc9ddfd03f9c9
jaehyeok <hyeok9sae@gmail.com>
2021-05-29 22:15:28 +0900

    21.05.29

--- BOJ_Python/BOJ_20327.py
+++ BOJ_Python/BOJ_20327.py
@@ -3,6 +3,10 @@ def check(k, l):
     for i in range(0, 2**N, 2**l):
         for j in range(0, 2**N, 2**l):
             rotate(i, j, 2**l, k)
+    if k == 5:
+        for i in range(2**N):
+            for j in range(2**N):
+                matrix[i][j] = lst[i][j]
 
 def rotate(row, col, size, k):
     tmp, gn = [], 0
@@ -29,9 +33,21 @@ def rotate(row, col, size, k):
             for j in range(row+size-1, row-1, -1):
                 matrix[j][i] = tmp[gn]
                 gn += 1
+    elif k == 5:
+        for i in range(2**N-(row+size), 2**N-(row+size)+size):
+            for j in range(col, col+size):
+                lst[i][j] = tmp[gn]
+                gn += 1
+    elif k == 6:
+        for i in range(row, row+size):
+            for j in range(col, col+size):
+                lst[i][j] = tmp[gn]
+                gn += 1
+
                 
 N, R = map(int, input().split())
 matrix = [[*map(int, input().split())] for _ in range(2**N)]
+lst = [[0]*(2**N) for _ in range(2**N)]
 for i in range(R):
     k, l = map(int, input().split())
     check(k, l)
