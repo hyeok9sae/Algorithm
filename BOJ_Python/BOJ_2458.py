3a704ed9c01c66e20ca7c4bd68e942a65f422e8e
jaehyeok <hyeok9sae@gmail.com>
2021-06-01 01:33:55 +0900

    21.06.01

--- BOJ_Python/BOJ_2458.py
+++ BOJ_Python/BOJ_2458.py
@@ -1,34 +1,19 @@
 # 키 순서
-from collections import deque
-
-def bfs(num):
-    visited[num] = True
-    deq = deque([num])
-    while deq:
-        number = deq.popleft()
-        for i in dic[number]:
-            if visited[i]:
-                continue
-            visited[i] = True
-            deq.append(i)
-
 N, M = map(int, input().split())
-dic = {}
-for i in range(1, N+1):
-    dic[i] = []
+matrix = [[0]*(N+1) for _ in range(N+1)]
 for _ in range(M):
     a, b = map(int, input().split())
-    dic[a].append(b)
+    matrix[a][b] = 1
+for k in range(1, N+1):
+    for i in range(1, N+1):
+        for j in range(1, N+1):
+            if matrix[i][k]+matrix[k][j] == 2:
+                matrix[i][j] = 1
 ans = 0
-for i in range(1, N+1):
-    flag = False
-    visited = [False]*(N+1)
-    bfs(i)
-    for j in range(1, len(visited)):
-        if not visited[j]:
-            flag = True
-            break
-    print(visited)
-    if not flag:
+for k in range(1, N+1):
+    cnt = 0
+    for i in range(1, N+1):
+        cnt += matrix[k][i] + matrix[i][k]
+    if cnt == N-1:
         ans += 1
 print(ans)
